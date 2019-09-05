aebd19f3c3f5fec7ab1708a64a701dce3b3b77d9
Daniel Wedin <danielwedin@hotmail.se>
2019-09-05 11:04:55 +0200

    Add nivo story

---
+++ README.md
@@ -0,0 +1,24 @@
+Demo of some React Graph libraries:
+
+Nivo
+https://github.com/plouc/nivo/
+GH stars: 5700, usedby 311
+
+Response from Bartek and Tomasz:
+For simple graphs - Nivo is a good choice.
+
+If you know that you need custom functionality, I will recommend you to do research with Nivo before you decide to use it
+They have struggled with custom functionality but were abled to achieve them.
+In my opinion, nivo is ok for simple charts, for more advanced - it requires a bit more coding and combinations
+
+Recharts:
+https://github.com/recharts/recharts
+GH stars: 12300, used by 18000
+
+Victory: (by FormidableLabs)
+https://github.com/FormidableLabs/victory
+GH stars: 7400, used by 3400
+
+react-vis (by uber)
+https://github.com/uber/react-vis
+GH stars: 6000, used by 3200
---
+++ stories/nivo.stories.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Line } from "@nivo/line";
+import { data } from "./demoData";
+
+const stories = storiesOf("Nivo Line", module);
+
+const max = data.map(point => point.y).sort()[0];
+
+stories.add("nivo", () => (
+  <Line
+    width={900}
+    height={400}
+    margin={{ top: 20, right: 20, bottom: 60, left: 80 }}
+    animate={true}
+    data={[
+      {
+        id: "asdfasdfasdf corp. A",
+        data: data,
+      },
+    ]}
+    xScale={{
+      type: "time",
+      format: "%s",
+      precision: "second",
+    }}
+    xFormat="time:%s"
+    yScale={{
+      type: "linear",
+      min: 0,
+      max: max * 2,
+    }}
+    axisLeft={{
+      legend: "time",
+      legendOffset: 12,
+      format: value => `${value / 1000}s`,
+    }}
+    axisBottom={{
+      format: "%b %d",
+      // tickValues: "every 2 days",
+    }}
+    curve="linear"
+    enablePoints={false}
+    useMesh={true}
+    colors={["#3498db"]}
+  />
+));
